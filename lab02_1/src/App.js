821c64076a6bb2f9b5f5870d086a8132207afb5a
Huu Thom <thomnhfx12107@funix.edu.vn>
2021-12-16 21:01:10 +0700

    lab02_2 update

--- lab02_1/src/App.js
+++ lab02_1/src/App.js
@@ -1,14 +1,13 @@
-
-import './App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
 function App() {
   return (
     <div className="App">
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
     </div>
   );
 }
