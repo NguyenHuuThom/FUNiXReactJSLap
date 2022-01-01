eaa036938b25191300a6af654c517d34ecf2bd91
Huu Thom <thomnhfx12107@funix.edu.vn>
2022-01-01 14:05:51 +0700

    Exercise 2: React Router và Single Page Applications

--- lab02_1/src/component/MenuComponent.js
+++ lab02_1/src/component/MenuComponent.js
@@ -20,7 +20,6 @@ function RenderMenuItem({ dish, onClick }) {
 }
 
 const Menu = (props) => {
-
   const menu = props.dishes.map((dish) => {
     return (
       <div className="col-12 col-md-5 m-1" key={dish.id}>
